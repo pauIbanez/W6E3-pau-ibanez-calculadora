4c47e86ba77fb55856ba6735bf093f12d9e959e7
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-16 15:36:32 +0100

    Added base for getting the numbers

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const { Command } = require("commander");
+// require("debug")("calculator");
 
+const program = new Command();
 
+program.option("-a, --first <number>");
+program.option("-b, --seccond <number>");
+program.parse();
